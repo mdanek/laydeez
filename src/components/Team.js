a33fd73b2cc0eee55aa7caa028dab7e3f1762245
danas <mateuszdanek@gmail.com>
2022-07-30 12:56:10 +0200

    team page and carousel added

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,24 +1,138 @@
-import TeamCard from "./TeamCard"
+import TeamCard from "./TeamCard";
+import Carousel from 'react-bootstrap/Carousel';
+import avatar1 from '../assets/team/avatar1.png';
+import avatar2 from '../assets/team/avatar2.png';
+import avatar3 from '../assets/team/avatar3.png';
+import avatar4 from '../assets/team/avatar4.png';
+import facebook from '../assets/social/facebook.svg';
+import twitter from '../assets/social/twitter.svg';
+import instagram from '../assets/social/instagram.svg';
+import discord from '../assets/social/discord.svg';
 import styles from './Team.module.css';
 
 const teamobj = [
     {
+        avatar: avatar1,
         name: "Lil Kim",
-        text: "She's got a booooty xD",
-        link: "google.com"
+        role: "She's got a booooty xD",
+        smlinks: [
+            {
+                name: "facebook",
+                link: "https://facebook.com",
+                icon: facebook
+            },
+            {
+                name: "twitter",
+                link: "https://twitter.com",
+                icon: twitter
+            },
+            {
+                name: "instagram",
+                link: "https://instagram.com",
+                icon: instagram
+            },
+            {
+                name: "discord",
+                link: "https://discord.com",
+                icon: discord
+            }
+        ]
     },
-    {},
-    {}
+    {
+        avatar: avatar2,
+        name: "Julia Roberts",
+        role: "Actress",
+        smlinks: [
+            {
+                name: "facebook",
+                link: "https://facebook.com",
+                icon: facebook
+            },
+            {
+                name: "twitter",
+                link: "https://twitter.com",
+                icon: twitter
+            },
+            {
+                name: "instagram",
+                link: "https://instagram.com",
+                icon: instagram
+            },
+            {
+                name: "discord",
+                link: "https://discord.com",
+                icon: discord
+            }
+        ]
+    },
+    {
+        avatar: avatar3,
+        name: "T Pain",
+        role: "Autotune master",
+        smlinks: [
+            {
+                name: "facebook",
+                link: "https://facebook.com",
+                icon: facebook
+            },
+            {
+                name: "twitter",
+                link: "https://twitter.com",
+                icon: twitter
+            },
+            {
+                name: "instagram",
+                link: "https://instagram.com",
+                icon: instagram
+            },
+            {
+                name: "discord",
+                link: "https://discord.com",
+                icon: discord
+            }
+        ]
+    },
+    {
+        avatar: avatar4,
+        name: "Yo Menik",
+        role: "Rapper",
+        smlinks: [
+            {
+                name: "facebook",
+                link: "https://facebook.com",
+                icon: facebook
+            },
+            {
+                name: "twitter",
+                link: "https://twitter.com",
+                icon: twitter
+            },
+            {
+                name: "instagram",
+                link: "https://instagram.com",
+                icon: instagram
+            },
+            {
+                name: "discord",
+                link: "https://discord.com",
+                icon: discord
+            }
+        ]
+    }
 ];
 
 const Team = () => {
 
     return (
         <div className={styles.team}>
-            <h1>Laydeez Team</h1>
-            <div>
-            {teamobj.map((teamcard, index) => 
-            (<TeamCard key={index} teamcard={teamcard} />))}
+            <div className={styles.teamHeader}></div>
+            <div className={styles.cards}>
+                <Carousel>
+                    {teamobj.map((member, index) => 
+                    (<Carousel.Item>
+                        <TeamCard key={index} member={member} />
+                    </Carousel.Item>))}
+                </Carousel>
             </div>
         </div>
     )
--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -2,16 +2,16 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import styles from './TeamCard.module.css';
 
-const TeamCard = ({teamcard}) => {
+const TeamCard = ({member}) => {
 
     return (
         <div>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" className="bi bi-emoji-smile" />
+            <Card style={{ width: '10rem' }}>
+                <Card.Img variant="top" src={member.avatar} className={styles.cardImg} />
                 <Card.Body>
-                    <Card.Title>{teamcard.name}</Card.Title>
+                    <Card.Title>{member.name}</Card.Title>
                     <Card.Text>
-                        {teamcard.text}
+                        {member.role}
                     </Card.Text>
                     <Button variant="primary">Go somewhere</Button>
                 </Card.Body>

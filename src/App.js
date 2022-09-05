159f48ec07e702c16929aba6a95c419e51bb4ee3
danas <mateuszdanek@gmail.com>
2022-09-05 22:01:28 +0200

    v6.4 new scroll and rainbow animation method

--- src/App.js
+++ src/App.js
@@ -16,51 +16,123 @@ import rainbowimg from './assets/transition3.png';
 
 function App() {
 
+  //Scrolling slides section -----------------------------------------
+  
+  //Variables used for scrolling and animating
   const scrollContainer = useRef(null);
-  const [toggleMenu, setToggleMenu] = useState(false);
-  const [isActive, setIsActive] = useState(false);
-  const [isScrolling, setIsScrolling] = useState(false);
+  const pagesContainer = useRef(null);
 
-  function reactToScrolling(e) {
-    
-      if (e.deltaY > 0) {
-        scrollContainer.current.scrollLeft += (1 * scrollContainer.current.offsetWidth);
-      // console.log(-1 * scrollContainer.current.offsetWidth);
-      } else {
-        scrollContainer.current.scrollLeft += (-1 * scrollContainer.current.offsetWidth);
-      // console.log(1 * scrollContainer.current.offsetWidth);
-      } 
+  let canScroll = true;
+  const delay = 2100;
 
-    // scrollContainer.current.scrollLeft += (e.deltaY/100 * scrollContainer.current.offsetWidth);
-    // console.log(e.deltaY/100 * scrollContainer.current.offsetWidth);
+  //Start ignoring unnecessary scroll events
+  const startIgnoringEvents = (delay) => {
+    console.log("started ignoring events");
+
+    setTimeout(() => {
+      canScroll = true;
+      console.log("Events not ignored anymore. Timeout canScroll: ", canScroll)
+    }, delay);
   }
 
+  //Execute full page horizontal scroll with rainbow animation
+  const executeScroll = (event, canScroll, delay) => {
+    let pageLeftSide = scrollContainer.current.scrollLeft;
+    let pageRightSide = scrollContainer.current.scrollLeft + scrollContainer.current.offsetWidth;
+    let fullContainerWidth = pagesContainer.current.offsetWidth;
+
+    //scroll right on mousewheel down after small timeout
+    if (event.deltaY > 0 && canScroll) {
+      setTimeout(() => {
+        scrollContainer.current.scrollLeft += (1 * scrollContainer.current.offsetWidth);
+      }, delay/10);
+      if(pageRightSide < fullContainerWidth) showRainbowAnimation(delay);
+    }
+
+    //scroll left on mousewheel up after small timeout
+    if (event.deltaY < 0 && canScroll) {
+      setTimeout(() => {
+        scrollContainer.current.scrollLeft += (-1 * scrollContainer.current.offsetWidth);
+      }, delay/10);
+      if(pageLeftSide > 0) showRainbowAnimation(delay);
+    }
+  }
+
+  //React to onWheel event
+  const reactToScrolling = (event) => {
+    if(canScroll) {
+      startIgnoringEvents(delay);
+      executeScroll(event, canScroll, delay);
+    }
+    canScroll = false;
+  }
+
+  //Rainbow animation on slide transition section ----------------------------
+  const animationContainer = useRef(null);
+  
+  const showRainbowAnimation = (delay) => {
+    const rainbow = animationContainer;
+  
+    if(!toggleMenu) {
+      rainbow.current.classList.add(styles.rainbowActive);
+      setTimeout(() => {
+        rainbow.current.classList.remove(styles.rainbowActive);
+      }, delay);
+    }
+    
+  }
+
+  //Hamburger menu section --------------------------------------------------
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  //Change hamburger menu open/close state
   const handleToggleMenu = () => {
     setToggleMenu(m => !m);
+    toggleRainbow(toggleMenu)
   }
 
+  //Toggle rainbow container on/off when menu is open
+  const toggleRainbow = (bool) => {
+    const rainbow = animationContainer;
+
+    //Check if menu is closed
+    if(bool) {
+      //Add rainbow class if there is none yet
+      if(!rainbow.current.classList.contains(styles.rainbow)) {
+        rainbow.current.classList.add(styles.rainbow);
+      }
+      //Remove rainbowOff class if there is one
+      if(rainbow.current.classList.contains(styles.rainbowOff)) {
+        rainbow.current.classList.remove(styles.rainbowOff);
+      }
+
+    //Remove rainbow class and add rainbowOff if menu is open
+    } else {
+        if(rainbow.current.classList.contains(styles.rainbow)) {
+          rainbow.current.classList.remove(styles.rainbow);
+        }
+        if(!rainbow.current.classList.contains(styles.rainbowOff)) {
+          rainbow.current.classList.add(styles.rainbowOff);
+        }
+    }
+  }
+
+  //Close hamburger menu when container is clicked
   const closeOnContainer = () => {
     if (toggleMenu) {
       handleToggleMenu();
     }
   }
 
-  const handleAnimation = () => {
-    setIsActive(m => !m);
-    // console.log("active is" + isActive);
-  }
 
-const pagesContainer = useRef(null);
-const animationContainer = useRef(null);
 
-useEffect(() => {
+/* useEffect(() => {
   const pages = pagesContainer.current.querySelectorAll('.page');
   const rainbow = animationContainer;
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting && !rainbow.current.classList.contains(styles.testActive) && scrollContainer.current.classList.contains(styles.closeMenu)) {
         rainbow.current.classList.add(styles.testActive);
-        // console.log("contains: ", scrollContainer.current.classList.contains(styles.closeMenu));
         return
       }
       setTimeout(() => {
@@ -76,15 +148,13 @@ useEffect(() => {
       observer.observe(page);
     }
   });
-}, [])
+}, []) */
 
+//Main app section -----------------------------------------------------------
   return (
     <div className={`${styles.app}`}>
-      <img ref={animationContainer} src={rainbowimg} className={`${styles.test}`}/>
-      <div onClick={closeOnContainer} ref={scrollContainer} onWheel={reactToScrolling} className={`${styles.mycontainer} ${toggleMenu ? styles.openMenu : styles.closeMenu}`}>
-        {/* <div className={styles.kwadrat}></div> */}
-        {/* <Header handleToggleMenu={handleToggleMenu}></Header> */}
-        
+      <img ref={animationContainer} src={rainbowimg} className={`${styles.rainbow}`}/>
+      <div onClick={closeOnContainer} ref={scrollContainer} onWheel={reactToScrolling} className={`${styles.mycontainer} ${toggleMenu ? styles.openMenu : styles.closeMenu}`}>        
         <div ref={pagesContainer} className={styles.page}>
           <Home handleToggleMenu={handleToggleMenu}></Home>
           <Team handleToggleMenu={handleToggleMenu}></Team>
@@ -93,8 +163,6 @@ useEffect(() => {
           <Baecoin handleToggleMenu={handleToggleMenu}></Baecoin>
           <Customize handleToggleMenu={handleToggleMenu}></Customize>
         </div>
-        {/* <img src={rainbowimg} className={`${styles.test} ${isActive ? styles.testActive : ''}`}/> */}
-        {/* <div ref={animationContainer} className={`${styles.test}`}></div>  */}
       </div>
       <Menu toggleMenu={toggleMenu} handleToggleMenu={handleToggleMenu}></Menu>
       
